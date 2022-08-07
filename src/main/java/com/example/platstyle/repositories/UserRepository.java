d6e1a7cdc9e8f609bfc3487141e5427437051e04
Sam Yang <yangsam810@gmail.com>
2022-08-06 22:15:53 -0700

    try to fix bug

--- src/main/java/com/example/platstyle/repositories/UserRepository.java
+++ src/main/java/com/example/platstyle/repositories/UserRepository.java
@@ -3,9 +3,12 @@ package com.example.platstyle.repositories;
 import com.example.platstyle.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAll();
     Optional<User> findByEmail(String email);
     Optional<User> findAllByUid(Long uid);
 
